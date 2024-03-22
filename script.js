fa329c102eb46017793320bf429a2f085b9e91d3
SirD3v4l07 <ascendantspiral@gmail.com>
2024-03-22 23:25:21 +0000

    Display submitted information

--- script.js
+++ script.js
@@ -10,3 +10,43 @@ document.querySelector("form").addEventListener("submit", function(event) {
         event.preventDefault();
     }
 });
+
+function submitForm () {
+    let filledForm = document.getElementById("signupForm");
+    let firstName = filledForm.elements["first-name"].value;
+    let lastName = filledForm.elements["last-name"].value;
+    let email = filledForm.elements["email"].value;
+    let phoneNumber = filledForm.elements["phone-number"].value;
+    let password = filledForm.elements["password"].value;
+    let heading = document.getElementById("heading");
+    heading.innerHTML = "Submitted Information: ";
+    filledForm.innerHTML = `
+    <form id="signupForm" action="#" method="post">
+                <div class="form-group">
+                    <div class="field-group">
+                        <label for="first-name">First Name</label>
+                        ${firstName}
+                    </div>
+                    <div class="field-group">
+                        <label for="last-name">Last Name</label>
+                        ${lastName}
+                    </div>
+                    <div class="field-group">
+                        <label for="email">Email</label>
+                        ${email}
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="field-group">
+                        <label for="phone-number">Phone Number</label>
+                        ${phoneNumber}
+                    </div>
+                    <div class="field-group">
+                        <label for="password">Password</label>
+                        ${password}
+                    </div>
+                    
+                </div>
+            </form>
+    `;
+}
